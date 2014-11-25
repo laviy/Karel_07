ba5cd3e4d63d8a11aa9c583843777d810897e3f3
laviy <yonatan_lavi@asl.org>
2014-11-25 12:33:37 +0000

    lab07

--- HillClimber.java
+++ HillClimber.java
@@ -11,22 +11,38 @@ import edu.fcps.karel2.Display;
 public class HillClimber extends Climber {
 
 	public HillClimber(int x) {
-		//TODO Call the appropriate superclass constructor
+      super(x);
 	}
 	
 	public void climbUpRight() {
-		//TODO Climb up one and across two
+		turnLeft();
+      move();
+      turnRight();
+      move();
+      move();
 	}
 	
 	public void climbUpLeft() {
-		//TODO Climb up one and across two
+      turnRight();
+      move();
+      turnLeft();
+      move();
+      move();
 	}
 	
 	public void climbDownRight() {
-		//TODO Climb down one and across two
+		move();
+      move();
+      turnRight();
+      move();
+      turnLeft();
 	}
 	
 	public void climbDownLeft() {
-		//TODO Climb down one and across two
+		move();
+      move();
+      turnLeft();
+      move();
+      turnRight();
 	}
 }
--- StepClimber.java
+++ StepClimber.java
@@ -10,22 +10,34 @@ import edu.fcps.karel2.Display;
 public class StepClimber extends Climber {
 
 	public StepClimber(int x) {
-		//TODO Call the appropriate superclass constructor
+		super(x);
 	}
 	
 	public void climbUpRight() {
-		//TODO Climb one step up and one across
+		turnLeft();
+      move();
+      turnRight();
+      move();
 	}
 	
 	public void climbUpLeft() {
-		//TODO Climb one step up and one across
+		turnRight();
+      move();
+      turnLeft();
+      move();
 	}
 	
 	public void climbDownRight() {
-		//TODO Climb one step down and one across
+		move();
+      turnRight();
+      move();
+      turnLeft();
 	}
 	
 	public void climbDownLeft() {
-		//TODO Climb one step down and one across
+		move();
+      turnLeft();
+      move();
+      turnRight();
 	}
 }
